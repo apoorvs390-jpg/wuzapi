10b22586885ce42bf43f0dbab09a66d8b560862c
apoorvs390-jpg <apoorvs390@gmail.com>
2026-06-24 13:54:54 +0530

    Update routes.go

--- routes.go
+++ routes.go
@@ -46,7 +46,19 @@ func (s *server) routes() {
 			Str("host", *address).
 			Logger()
 	}
-
+s.router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Token, Authorization")
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	})
+	
 	s.router.Handle("/health", s.GetHealth()).Methods("GET")
 
 	adminRoutes := s.router.PathPrefix("/admin").Subrouter()
